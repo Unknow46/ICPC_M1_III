6911fc98fc6e5480bd31f073d477b7c2aba37d5e
Nathan Sens <49709624+Unknow46@users.noreply.github.com>
2020-05-19 12:54:07 +0200

    :construction: prototype function landing

--- Projets/carefulApproach/carefulApproach.cpp
+++ Projets/carefulApproach/carefulApproach.cpp
@@ -1,3 +1,20 @@
 #include <algorithm>
 #include <cmat>
 #include <cstdio>
+
+
+int i, n, order[8];
+double a[8], b[8], L;
+
+double AtterisageDangereux() {
+  double dernierAtte = a[order[0]];     
+  for (i = 1; i < n; i++) {                      
+    double cibleAtteTemps = dernierAtte + L;
+    if (cibleAtteTemps <= b[order[i]])
+      dernierAtte = max(a[order[i]], cibleAtteTemps);
+    else
+      return 1;
+  }
+  return dernierAtte - b[order[n - 1]];
+}
+
